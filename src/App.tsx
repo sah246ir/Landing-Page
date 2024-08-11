450ef1861b70adccb7b2527d4b54eb5d38432777
sahir <sahir@pop-os.localdomain>
2024-08-11 11:46:04 +0530

    fix: babel errorzzzzz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react'; 
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Button, { ArrowButton } from './components/Button';
+import Footer from './components/Footer/Footer'; 
 import ContactSection from './components/Contact/ContactSection';
 import StatisticsSection from './components/Statistics/StatisticsSection';
 import About from './components/Aboutus/About';
