36938ad193f8f4dea081b09b2f78375b1bef6cfa
sahir <sahir@pop-os.localdomain>
2024-08-20 12:36:14 +0530

    refactor: using input comp

--- src/components/HomePageComponents/Contact/RightContact.tsx
+++ src/components/HomePageComponents/Contact/RightContact.tsx
@@ -1,30 +1,31 @@
 import Button from '../../Button'
+import { InputGroup } from '../../Input'
 
 const RightContact = () => {
   return (
     <form className='flex flex-col gap-4'>
       <div className='flex gap-4 flex-col sm:flex-row'>
-        <div className='flex flex-col gap-1'>
-          <label className='text-md text-gray-700 font-light' htmlFor="firstname">First Name</label>
-          <input className='border border-black px-2 py-1.5' placeholder='First name' type="text" name="firstname" id="firstname" />
-        </div>
+        <InputGroup
+          placeholder='First name' type="text" name="firstname" id="firstname"
+          label='First name'
+        />
 
-        <div className='flex flex-col gap-1'>
-          <label className='text-md text-gray-700 font-light' htmlFor="lastname">Last Name</label>
-          <input className='border border-black px-2 py-1.5' placeholder='Last name' type="text" name="lastname" id="lastname" />
-        </div>
+        <InputGroup
+          placeholder='Last name' type="text" name="lastname" id="lastname"
+          label='Last name'
+        />
       </div>
 
       <div className='flex gap-4 flex-col sm:flex-row'>
-        <div className='flex flex-col gap-1'>
-          <label className='text-md text-gray-700 font-light' htmlFor="email">Email</label>
-          <input className='border border-black px-2 py-1.5' placeholder='abc@company.com' type="text" name="email" id="email" />
-        </div>
+        <InputGroup
+          placeholder='Email' type="email" name="email" id="email"
+          label='Email'
+        />
 
-        <div className='flex flex-col gap-1'>
-          <label className='text-md text-gray-700 font-light' htmlFor="mobile">Mobile</label>
-          <input className='border border-black px-2 py-1.5' placeholder='xxx-xxx-xxxx' type="text" name="phone" id="phone" />
-        </div>
+        <InputGroup
+          placeholder='Mobile' type="mobile" name="mobile" id="mobile"
+          label='Mobile'
+        />
       </div>
 
       <textarea rows={5} className='border border-black px-2 py-1.5 w-full' placeholder='Message' name="msg" id="msg"></textarea>
