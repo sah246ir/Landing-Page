97adb2495394e14ade1f53dffcf91f79362f0a0f
sahir <sahir@pop-os.localdomain>
2024-08-20 16:45:59 +0530

    feat: input groups and textarea

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,7 +9,7 @@ interface PropTypes {
 }
 const Input = ({ className, type, name, id, placeholder }: PropTypes) => {
     return (
-        <input className={'border border-black px-2 py-1.5 ' + className}
+        <input className={'border border-gray-400 px-2 py-1.5 rounded-sm ' + className}
             placeholder={placeholder}
             type={type}
             name={name}
@@ -26,8 +26,8 @@ interface GroupPropTypes extends PropTypes {
 export const InputGroup = ({ className, type, name, id, placeholder, labelClassName, inputClassName, label }: GroupPropTypes) => {
     return (
         <div className={'flex flex-col gap-1 ' + className}>
-            <label className={'text-md text-gray-700 font-light ' + labelClassName} htmlFor={id}>{label}</label>
-            <input className={'border border-black px-2 py-1.5 ' + inputClassName}
+            <label className={'text-md text-gray-600 font-medium ' + labelClassName} htmlFor={id}>{label}</label>
+            <Input className={'border border-gray-400 px-2 py-1.5 rounded-sm ' + inputClassName}
                 placeholder={placeholder}
                 type={type}
                 name={name}
@@ -35,5 +35,39 @@ export const InputGroup = ({ className, type, name, id, placeholder, labelClassN
         </div>
     )
 }
+
+interface TextareaProps extends Omit<PropTypes,"type">{
+    rows?:number
+}
+export const Textarea = ({ className, rows, name, id, placeholder }: TextareaProps) => {
+    return (
+        <textarea className={'border border-gray-400 px-2 py-1.5 rounded-sm ' + className}
+            placeholder={placeholder}
+            name={name}
+            id={id} 
+            rows={rows}
+            />
+    )
+}
+
+interface TextareaGroupPropTypes extends TextareaProps {
+    labelClassName?: string
+    inputClassName?: string
+    label?: string
+
+}
+export const TextareaGroup = ({ className, rows, name, id, placeholder, labelClassName, inputClassName, label }: TextareaGroupPropTypes) => {
+    return (
+        <div className={'flex flex-col gap-1 ' + className}>
+            <label className={'text-md text-gray-600 font-medium ' + labelClassName} htmlFor={id}>{label}</label>
+            <Textarea className={'border border-gray-400 px-2 py-1.5 rounded-sm ' + inputClassName}
+                placeholder={placeholder}
+                rows={rows}
+                name={name}
+                id={id} />
+        </div>
+    )
+}
+
 
 export default Input
