a4580933b2dba4e60d0a45b4d5ab5cb1dae9c3c4
sahir <sahir@pop-os.localdomain>
2024-08-20 16:46:37 +0530

    feat: accordian component

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,30 @@
+import { ChevronDown } from 'lucide-react';
+import React, { useState } from 'react';
+
+interface PropTypes {
+    className?: string;
+    children: React.ReactNode;
+    title: string;
+} 
+
+export const Accordion = ({ className,children, title }: PropTypes) => {
+    const [open,Setopen] = useState<boolean>(false)
+    return (
+        <div className={`p-2 px-4 ${className}`}>
+            <div onClick={()=>Setopen(prev=>!prev)} className="flex justify-between text-gray-800 text-lg">
+                <p>{title}</p>
+                <i><ChevronDown stroke='gray' /></i>
+            </div>
+
+            <div className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
+                open?"grid-rows-[1fr] opacity-100":"grid-rows-[0fr] opacity-0 "
+            }`}>
+                <div className='overflow-hidden'>
+                    <div className="py-2">{children && children}</div>
+                </div>  
+            </div>
+        </div>
+    );
+};
+
+ 
