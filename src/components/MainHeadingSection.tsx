3830d3eb57f93ba3cadaebc2230f051d4764ff29
sahir <sahir@pop-os.localdomain>
2024-08-20 12:36:39 +0530

    styke:bold

--- src/components/MainHeadingSection.tsx
+++ src/components/MainHeadingSection.tsx
@@ -11,7 +11,7 @@ interface PropTypes{
 const MainHeadingSection = ({title,description,children,className}:PropTypes) => {
     return (
         <Section padded center className={'flex-col bg-gray-50 min-h-96 text-center ' + className}>
-            <Heading className='text-5xl font-normal text-center mb-4'>{title}</Heading>
+            <Heading className='text-5xl font-medium text-center mb-4'>{title}</Heading>
             {description && <p className='text-center  sm:text-base text-xs text-gray-600 leading-loose'>{description}</p>}
             {children && children}
         </Section>
