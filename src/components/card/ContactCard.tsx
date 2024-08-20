8fc1313e92fe32784b33b5f43c7b2c59ded952c3
sahir <sahir@pop-os.localdomain>
2024-08-20 12:02:25 +0530

    feat: dynamic icons

--- src/components/card/ContactCard.tsx
+++ src/components/card/ContactCard.tsx
@@ -1,19 +1,27 @@
-import { Contact } from 'lucide-react'
-import React from 'react'
+import { Contact,  } from 'lucide-react'
+import React, { lazy, Suspense } from 'react'
+import dynamicIconImports from 'lucide-react/dynamicIconImports';
 
-interface PropTypes{
-    title:string,
-    description:string
-    className?:string
+interface PropTypes {
+    title: string,
+    description: string
+    className?: string
+    iconClassName?: string
+    icon?: keyof typeof dynamicIconImports
 }
-const ContactCard = ({title,description,className}:PropTypes) => {
+
+const fallback = <Contact strokeWidth={1.2} size={35} />
+const ContactCard = ({ title, description, className, iconClassName, icon }: PropTypes) => {
+    const LucideIcon = icon ? lazy(dynamicIconImports[icon]) : undefined;
     return (
         <div className={'border p-6 bg-white sm:w-[15em] grid place-items-center pt-0  flex-col gap-3 rounded-lg ' + className}>
 
-            <i className='size-16 bg-gray-300 text-white grid place-items-center rounded-full -translate-y-6'>
-                <span className="sr-only">Contact icon</span>
-                <Contact className='text-gray- ' size={35} />
-            </i>
+                <i className={'size-16 bg-gray-300 text-white grid place-items-center rounded-full -translate-y-6 ' + iconClassName}>
+                    <span className="sr-only">Contact icon</span>
+                    <Suspense fallback={fallback}>
+                     {LucideIcon?<LucideIcon strokeWidth={1.2} size={35} />:fallback  }
+                    </Suspense>
+                </i>
 
             <h1 className='font-semibold mt-[-1.5rem]'>{title}</h1>
             <p className='text-xs'>{description}</p>
