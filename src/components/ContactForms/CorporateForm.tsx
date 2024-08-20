4492e7b0e816e2e646e9ca7b39bf7a50e580f88b
sahir <sahir@pop-os.localdomain>
2024-08-20 16:49:56 +0530

    fix: linting

--- src/components/ContactForms/CorporateForm.tsx
+++ src/components/ContactForms/CorporateForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { InputGroup, Textarea, TextareaGroup } from '../Input'
+import { InputGroup,  TextareaGroup } from '../Input'
 import Button from '../Button'
 
 const CorporateForm = () => {
