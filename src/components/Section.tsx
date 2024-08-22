00a05f08246b506fc81b46d7c1e2971f58feae6b
sahir <sahir@pop-os.localdomain>
2024-08-22 10:49:20 +0530

    style: increaseing default padding

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -10,7 +10,7 @@ interface Props{
 }
 const Section = ({padded,center,children,className,style,gray=true}:Props) => {
   return (
-    <section style={{...style}} className={`flex ${padded?"py-12 p-5 ":" "} ${center?"items-center justify-center ":""} ${gray?"bg-gray-50":" bg-white"} ${className}`}>
+    <section style={{...style}} className={`flex ${padded?"py-24 p-5 ":" "} ${center?"items-center justify-center ":""} ${gray?"bg-gray-50":" bg-white"} ${className}`}>
       {children && children}
     </section>
   )
