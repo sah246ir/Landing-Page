493866674dc67f4e6b9205b2e94e4f7a1f423a82
sahir <sahir@pop-os.localdomain>
2024-08-22 10:34:12 +0530

    style: bolder head

--- src/components/MainHeadingSection.tsx
+++ src/components/MainHeadingSection.tsx
@@ -12,7 +12,7 @@ interface PropTypes{
 const MainHeadingSection = ({title,description,children,className,gray}:PropTypes) => {
     return (
         <Section gray={gray} padded center className={'flex-col bg-gray-50 min-h-96 text-center max-w-[1100px]' + className}>
-            <Heading className='text-5xl font-normal text-center mb-4'>{title}</Heading>
+            <Heading className='text-5xl font-medium text-center mb-4'>{title}</Heading>
         {description && <p className='text-center sm:w-[65%] sm:text-sm text-xs text-gray-600 leading-loose'>{description}</p>}
             {children && children}
         </Section>
