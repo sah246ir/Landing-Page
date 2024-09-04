3b1d5484811780b55f721b3d976547c8b05eb3e1
sahir <sahir@pop-os.localdomain>
2024-09-04 14:21:08 +0530

    feat: props

--- src/components/InvestmentsComponents/ServiceCard.tsx
+++ src/components/InvestmentsComponents/ServiceCard.tsx
@@ -3,15 +3,20 @@ import React from 'react'
 import Heading from '../heading'
 import SubHeading from '../SubHeading'
 
-const ServiceCard = () => {
+interface PropTypes{
+    icon?:any
+    heading?:string
+    content?:string
+}
+const ServiceCard = ({heading,content}:PropTypes) => {
     return (
         <div className='transition flex flex-col gap-2 bg-gray-50 border border-gray-300 p-4 py-6 sm:w-[13em] hover:bg-white'>
             <GaugeCircle className='text-gray-500' size={30} />
-            <Heading className='font-normal text-lg !m-0'>Demo Service</Heading>
-            <SubHeading className=' text-gray-600' >Are you doing the same tasks
-                repeatedly? Leave it with us,
+            <Heading className='font-normal text-lg !m-0'>{heading || "Demo Service"}</Heading>
+            <SubHeading className=' text-gray-600' >{content || `Are you doing the same tasks
+                repeatedly Leave it with us,
                 we'll put a robot in place to do it
-                for you!</SubHeading>
+                for you!`}</SubHeading>
         </div>
     )
 }
