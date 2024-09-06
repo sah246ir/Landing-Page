59e669058398051f39a19f2f2a948e05e02cebbf
sahir <sahir@pop-os.localdomain>
2024-09-06 12:30:42 +0530

    fix: linting

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,6 @@ import About from '../components/HomePageComponents/Aboutus/About'
 import PortfolioSection from '../components/HomePageComponents/Portfolio/PortfolioSection'
 import ServiceSection from '../components/HomePageComponents/Services/ServicesSection'
 import ContactSection from '../components/HomePageComponents/Contact/ContactSection'
-import BlogSection from '../components/HomePageComponents/Blog/BlogSection'
 import HistorySection from '../components/HomePageComponents/History/HistorySection'
 import StatisticsSection from '../components/HomePageComponents/Statistics/StatisticsSection'
 
