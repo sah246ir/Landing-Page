366af96878eda689e1855afa0cf9388059704353
sahir <sahir@pop-os.localdomain>
2024-09-09 15:13:44 +0530

    style: full width

--- src/components/MainHeadingSection.tsx
+++ src/components/MainHeadingSection.tsx
@@ -14,9 +14,9 @@ interface PropTypes {
 const MainHeadingSection = ({ img,title, description, children, className, gray }: PropTypes) => {
     const imgg = img?require("../img/"+img):undefined
     return (
-        <Section gray={gray} padded center className={'flex-col bg-gray-50 min-h-96 text-center max-w-[1100px] relative ' + className}>
+        <Section gray={gray} padded center className={'flex-col bg-gray-50 min-h-96 text-center w-full relative ' + className}>
             
-            {imgg && <img src={imgg} alt="" className='absolute w-full h-full  ' />}
+            {imgg && <img src={imgg} alt="" className='absolute w-full h-full object-cover  ' />}
             {imgg && <div className="absolute w-full h-full bg-black opacity-50"></div> }
             <Heading className={'text-5xl font-medium text-center mb-4 z-30 ' + (imgg && "text-white ")}>{title}</Heading>
             {description && <SubHeading className={'text-center sm:w-[75%] sm:text-lg text-sm z-30 text-gray-600 leading-loose ' + (imgg?"text-white":"")}>{description}</SubHeading>}
