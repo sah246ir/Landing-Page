cf1a90b6425d984cbbb7fbd2a29a59524f9e1fdd
sahir <sahir@pop-os.localdomain>
2024-09-09 13:58:04 +0530

    style: navbar colored and fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from './Sidebar'
 import HamburgerIcon from './HamburgerIcon'
 const logo = require("../img/logo.jpg")
 const Navbar = ({ className }: { className?: string }) => {
     const [isopen, setIsOpen] = useState<boolean>(false)
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <>
-            <nav className={'w-full   top-0 flex justify-between px-4 py-2 ' + className}>
-                <img alt='' width={115} src={logo} className='text-4xl font-normal' /> 
-
+            <Sidebar setIsOpen={setIsOpen} isopen={isopen} />
+            <nav className={'w-full sticky z-50   top-0 flex justify-between items-center px-4 py-3 bg-white ' + className}>
+                <div><img alt='' width={scrolled?75:115} src={logo} className='text-4xl font-normal transition-all duration-300' />  </div>
                 <HamburgerIcon isopen={isopen} setIsOpen={setIsOpen}/>
             </nav>
-            <Sidebar setIsOpen={setIsOpen} isopen={isopen} />
         </>
     )
 }
