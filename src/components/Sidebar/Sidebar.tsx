7f9ce236cd1d666fddce01ae2b33936af589a378
sahir <sahir@pop-os.localdomain>
2024-09-11 19:57:53 +0530

    refactor: moving sidebar action in seperate file

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,30 +1,9 @@
-import React from 'react'
-import Button from '../Button'
-import { Link } from 'react-router-dom'
+import React from 'react' 
+import SidebarAction from './SidebarAction'
 
 
-
-
-
-
-
-
-interface ActionProps{
-  title:string,
-  href:string,
-  onclick:Function
-}
-export const SidebarAction = ({title,href,onclick}:ActionProps)=>{
-  return( 
-    <Link className='text-left w-full text-blue-900 hover:text-blue-700' to={href}> 
-      <Button onClick={()=>onclick((prev: boolean)=>!prev)} className='text-left w-full transition duration-200 flex-grow border-0 border-b-2 border-gray-200 hover:border-gray-100 text-2xl tracking-widest !font-normal'>
  
-        {title}
-      </Button>
-    </Link>
 
-  )
-}
 interface PropTypes {
   isopen?: boolean
   setIsOpen:Function
