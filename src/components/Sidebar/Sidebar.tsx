57177e5ad6c23c88f25098f3d206ccc85dfa2e61
sahir <sahir@pop-os.localdomain>
2024-09-11 21:18:20 +0530

    refactor: services as dropdown

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,9 @@
 import React from 'react' 
 import SidebarAction from './SidebarAction'
+import Dropdown from '../dropdown/Dropdown'
+import DropdownTitle from '../dropdown/DropdownTitle'
+import DropdownContent from '../dropdown/DropdownContent'
+import DropdownItem from '../dropdown/DropdownItem'
 
 
  
@@ -10,13 +14,20 @@ interface PropTypes {
 }
 const Sidebar = ({ isopen,setIsOpen }: PropTypes) => {
   return (
-    <div className={`whitespace-nowrap z-50 transition-all duration-300 ease-linear fixed  bg-white bg-opacity-95 sm:bg-opacity-100 top-0 right-0 min-h-screen ${isopen ? "w-full sm:w-[25%]" : "w-0"} flex items-center justify-center overflow-hidden`}>
+    <div className={`whitespace-nowrap z-50 transition-all duration-300 ease-linear fixed  bg-white bg-opacity-95 sm:bg-opacity-100 top-0 right-0 min-h-screen ${isopen ? "w-full sm:w-[35%]" : "w-0"} flex items-center justify-center overflow-hidden`}>
 
       <div className="flex flex-col gap-8  w-full p-8">
        <SidebarAction onclick={setIsOpen} title='Home' href='/' />
        <SidebarAction onclick={setIsOpen} title='About us' href='/about-us' />
-       <SidebarAction onclick={setIsOpen} title='investments' href='/investments' />
-       <SidebarAction onclick={setIsOpen} title='Services' href='/services' />
+       <Dropdown>
+          <DropdownTitle className='text-blue-900 hover:text-blue-700'>
+            Services
+          </DropdownTitle>
+          <DropdownContent>
+            <DropdownItem onClick={()=>setIsOpen(false)} className='text-blue-400 hover:text-blue-700' to='/investments'>Investments and strategic consulting</DropdownItem>  
+            <DropdownItem onClick={()=>setIsOpen(false)} className='text-blue-400 hover:text-blue-700' to='/services'>Other Services</DropdownItem>  
+          </DropdownContent>
+       </Dropdown>
        <SidebarAction onclick={setIsOpen} title='Contact' href='/contact-us' />
       </div>
 
