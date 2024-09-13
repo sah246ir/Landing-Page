96d26bea6cc930ef543bce98dc395bc303f31b52
sahir <sahir@pop-os.localdomain>
2024-09-13 16:22:14 +0530

    fix: updating footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,11 @@
-import NewsLetterFooter from './NewsLetterFooter'
 import FooterContent from './FooterContent'
 
 const Footer = () => {
   return (
     <div className="bg-blue-900 p-5 flex justify-center">
-      <div className='w-[100%] lg:w-[1100px]'>
-        <NewsLetterFooter />
-        <hr className="my-5  border-blue-900 h-[1px]" />
+      <div className='w-[100%] lg:w-[1100px] py-5'>
+        {/* <NewsLetterFooter /> */}
+        {/* <hr className="my-5  border-blue-900 h-[1px]" /> */}
         <FooterContent />
       </div>
     </div>
