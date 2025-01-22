3ff175920c22aebf971a0f9610ee3fef93e4fbf5
sahir <sahir@pop-os.localdomain>
2025-01-22 14:58:33 +0530

    form

--- src/components/ContactForms/CorporateForm.tsx
+++ src/components/ContactForms/CorporateForm.tsx
@@ -3,8 +3,28 @@ import { InputGroup,  TextareaGroup } from '../Input'
 import Button from '../Button'
 
 const CorporateForm = () => {
+  const handleSubmit = (event:React.FormEvent) => {
+    event.preventDefault();
+  
+    const myForm = event.target as HTMLFormElement;
+    const formData = new FormData(myForm);
+    const formDataParams = new URLSearchParams();
+    
+    formData.forEach((value, key) => {
+      formDataParams.append(key, value.toString());
+    });
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: formDataParams.toString()
+    })
+      .then(() => alert("/thank-you/"))
+      .catch(error => alert(error));
+  };
+  
   return (
-    <form className='flex flex-col gap-4' name="contact" data-netlify="true" netlify-honeypot="bot-field" method='post'>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4' name="contact" data-netlify="true" netlify-honeypot="bot-field" method='post'>
+          <input type="hidden" name="form-name" value="contact" />
         <InputGroup
           placeholder='First name' type="text" name="firstname" id="firstname"
           label='First name'
